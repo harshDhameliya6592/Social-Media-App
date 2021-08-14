5723fc6a21c9c7ddf7b2ad11f21a65008bd8974c
Harsh Dhameliya <harshdhameliya6592@gmail.com>
2021-08-14 13:20:47 +0530

    Social Media App

---
+++ app/src/main/java/com/example/socialapp/models/User.kt
@@ -0,0 +1,7 @@
+package com.example.socialapp.models
+
+import android.view.displayhash.DisplayHash
+
+class User (val uid:String ="",
+            val displayName:String ?="",
+            val imageUrl:String="")
